55e2842a3dc85fd78a9519289ec72c7aadebc1c6
itsnow <xue.com.fei@qq.com>
2018-02-27 15:26:53 +0800

    update

--- app/src/main/java/com/upgradedemo/MainActivity.java
+++ app/src/main/java/com/upgradedemo/MainActivity.java
@@ -1,7 +1,10 @@
 package com.upgradedemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.upgradelibrary.UpgradeHelper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final UpgradeHelper helper = new UpgradeHelper(MainActivity.this);
+        findViewById(R.id.button_check).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                helper.checkForUpdates("http://www.rainen.cn/test/app-update.xml", false);
+            }
+        });
+        findViewById(R.id.button_cancle).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                helper.cancel();
+            }
+        });
     }
 }
